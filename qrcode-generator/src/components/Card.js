205d6b946ec3194d8e36ffdcc63c036a39ed11fe
Guga Macedo <94029266+gugamacedo@users.noreply.github.com>
2022-04-11 20:02:58 -0300

    data verification and api integration

--- qrcode-generator/src/components/Card.js
+++ qrcode-generator/src/components/Card.js
@@ -1,55 +1,94 @@
-import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+
 import { makeStyles } from '@mui/styles'
-import { Button, TextField } from '@mui/material'
-import { useState } from 'react'
-import { Box } from '@mui/system'
+import Image from 'next/image'
+import { Button, TextField, Box } from '@mui/material'
 
 const useStyles = makeStyles(() => ({
   image: {
     margin: '30px 0',
   },
 
-  wrapper: {
-    display: 'flex',
-    alignItems: 'center',
+  form: {
     width: '50%',
-    gap: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+
+    '& button': {
+      margin: '30px 0 0',
+    }
   },
 }))
+
+const validationSchema = yup.object().shape({
+  website: yup.string().required('Campo obrigatório').url('URL inválida'),
+})
 
 const Card = () => {
   const classes = useStyles()
   const [loading, setLoading] = useState(false)
 
-  const handleInputChange = () => {}
-  const handleFormSubmit = () => {}
+  const [image, setImage] = useState('/favicon.svg')
+
+  useEffect(() => {
+    setImage(image)
+  }
+    
+  , [image])
 
   return (
     <>
       <Box className={classes.image}>
         <Image
-          src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&bgcolor=424242&color=00ff88&data=https://mui.com/pt/components/"
+          src={image}
           alt="QrCode"
           width={'300px'}
           height={'300px'}
         />
       </Box>
 
-      <div className={classes.wrapper}>
-        <TextField
-          size="small"
-          fullWidth
-          label="Enter the link"
-          name="link"
-          variant="outlined"
-          onChange={handleInputChange}
-          type="url"
-        />
+      <Formik
+        initialValues={{
+          website: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          setLoading(true)
 
-        <Button variant="contained" color="primary" onClick={handleFormSubmit}>
-          {loading ? 'Aguarde...' : 'Generate'}
-        </Button>
-      </div>
+          axios.get(`https://api.qrserver.com/v1/create-qr-code/?size=300x300&bgcolor=424242&color=00ff88&data=${values.website}`)
+          .then(response => {
+            const data = response.request.responseURL
+            setImage(data)
+            setLoading(false)
+          })
+
+        }}
+      >
+        {({ values, errors, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit} className={classes.form}>
+            <TextField
+              size="small"
+              fullWidth
+              label="Enter the url"
+              variant="filled"
+              placeholder="https://github.com"
+              onChange={handleChange}
+              name="website"
+              value={values.website}
+              error={errors.website}
+              helperText={errors.website}
+            />
+
+            <Button variant="contained" color="primary" type="submit">
+              {loading ? 'Wait...' : 'Generate'}
+            </Button>
+          </form>
+        )}
+      </Formik>
     </>
   )
 }
